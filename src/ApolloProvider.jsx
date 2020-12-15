909287559d296d2ca1fa066c4e2893a48b7e4c8b
DA_RT <slobro@mail.ru>
2020-12-15 08:10:26 +0300

    edited proxy and server base URL

--- src/ApolloProvider.jsx
+++ src/ApolloProvider.jsx
@@ -7,7 +7,8 @@ import {PlayerState} from "./context/playerState";
 import {Theme} from "./components/Theme/Theme";
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    // uri: 'http://localhost:5000'
+    uri: 'https://evening-brook-36733.herokuapp.com/'
 })
 
 const client = new ApolloClient({
